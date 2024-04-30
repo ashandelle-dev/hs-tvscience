b709b93b2039260efc4b3cf70bdf5b546f3be668
bobbiejwilson <poldira@gmail.com>
2024-04-30 17:07:53 -0500

    Adding style change module - by Will

---
+++ src/modules/style-change.module/module.js
@@ -0,0 +1,13 @@
+ <script>
+    document.addEventListener("DOMContentLoaded", function() {
+      // Get references to the images
+      const image1 = document.getElementById('image1');
+      const image2 = document.getElementById('image2');
+      const image3 = document.getElementById('image3');
+
+      // Replace image sources
+      image1.src = "https://8975772.fs1.hubspotusercontent-na1.net/hubfs/8975772/top-right-bgp.png";
+      image2.src = "https://8975772.fs1.hubspotusercontent-na1.net/hubfs/8975772/bottom-left-bgp.png";
+      image3.src = "https://8975772.fs1.hubspotusercontent-na1.net/hubfs/8975772/cta-image.png";
+    });
+  </script>
