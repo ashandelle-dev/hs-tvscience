03492dd39f299508e9e43553ce0f53ba66261e65
bobbiejwilson <poldira@gmail.com>
2025-04-23 18:36:48 -0500

    CTA module changes, updates to style module

--- src/modules/style-change.module/module.js
+++ src/modules/style-change.module/module.js
@@ -5,7 +5,7 @@
       const image3 = document.getElementById('image3');
 
       // Replace image sources
-      image1.src = "https://8975772.fs1.hubspotusercontent-na1.net/hubfs/8975772/top-right-bgp.png";
-      image2.src = "https://8975772.fs1.hubspotusercontent-na1.net/hubfs/8975772/bottom-left-bgp.png";
-      image3.src = "https://8975772.fs1.hubspotusercontent-na1.net/hubfs/8975772/cta-image.png";
+      //image1.src = "https://8975772.fs1.hubspotusercontent-na1.net/hubfs/8975772/top-right-bgp.png";
+      //image2.src = "https://8975772.fs1.hubspotusercontent-na1.net/hubfs/8975772/bottom-left-bgp.png";
+      //image3.src = "https://8975772.fs1.hubspotusercontent-na1.net/hubfs/8975772/cta-image.png";
     });
